14001a89db41d4cf11623a6fc6611fd5bfeef3c2
bobyjoe111 <38360525+bobyjoe111@users.noreply.github.com>
2022-04-09 15:41:12 -0600

    Update allthecode.js

--- allthecode.js
+++ allthecode.js
@@ -1,3 +1,258 @@
+// Nueral Network
+function table(arr) {
+    var string = '';
+    for (var i = 0; i < arr.data.length; i++) {
+        for (var j = 0; j < arr.data[i].length; j++) {
+            string += arr.data[i][j] + ', ';
+        }
+        string += '\n';
+    }
+    
+    return string;
+}
+
+function Matrix(rows, cols) {
+    this.rows = rows;
+    this.cols = cols;
+    this.data = [];
+    
+    this.table = function() {
+        var string = '';
+        for (var i = 0; i < this.data.length; i++) {
+            for (var j = 0; j < this.data[i].length; j++) {
+                string += this.data[i][j] + ', ';
+            }
+            string += '\n';
+        }
+	return string;
+    };
+    
+    for (var i = 0; i < this.rows; i++) {
+        this.data[i] = [];
+        for (var j = 0; j < this.cols; j++) {
+            this.data[i][j] = 0;
+        }
+    }
+    
+    this.toArray = function(a) {
+        var arr = [];
+        for (var i = 0; i < this.rows; i++) {
+            for (var j = 0; j < this.cols; j++) {
+                arr.push(this.data[i][j]);
+            }
+        }
+        return arr;
+    };
+    
+    this.print = function() {
+        return table(this.data) + "\n";
+    };
+    
+    this.transpose = function() {
+        var result = new Matrix(this.cols, this.rows);
+        for (var i = 0; i < this.rows; i++) {
+            for (var j = 0; j < this.cols; j++) {
+                result.data[j][i] = this.data[i][j];
+            }
+        }
+        this.rows = result.rows;
+        this.cols = result.cols;
+        this.data = result.data;
+    };
+    
+    this.randomize = function() {
+        for (var i = 0; i < this.rows; i++) {
+            for (var j = 0; j < this.cols; j++) {
+                this.data[i][j] = Math.random() * Math.random() * (1 - (-1)) + -1;
+            }
+        }
+    };
+    
+    this.multiply = function(n) {
+        if (n instanceof Matrix) {
+            for (var i = 0; i < this.rows; i++) {
+                for (var j = 0; j < this.cols; j++) {
+                    this.data[i][j]*=n.data[i][j];
+                }
+            }  
+            return;
+        }
+        for (var i = 0; i < this.rows; i++) {
+            for (var j = 0; j < this.cols; j++) {
+                this.data[i][j]*=n;
+            }
+        } 
+    };
+    
+    this.map = function(fn) {
+        for (var i = 0; i < this.rows; i++) {
+            for (var j = 0; j < this.cols; j++) {
+                var value = this.data[i][j];
+                this.data[i][j] = fn(value);
+            }
+        } 
+    };
+    
+    this.add = function(n) {
+        if (n instanceof Matrix) {
+            for (var i = 0; i < this.rows; i++) {
+                for (var j = 0; j < this.cols; j++) {
+                    this.data[i][j] += n.data[i][j];
+                }
+            }
+        } else {
+            for (var i = 0; i < this.rows; i++) {
+                for (var j = 0; j < this.cols; j++) {
+                    this.data[i][j] += n;
+                }
+            }    
+        }
+    };
+}
+
+Matrix.fromArray = function(a) {
+    var result = new Matrix(a.length, 1);
+    for (var i = 0; i < a.length; i++) {
+        result.data[i][0] = a[i];
+    }
+    return result;
+};
+
+Matrix.multiply = function(a, b){
+    if (a.cols !== b.rows) {
+        return new Error('Numbe of columns must match number of rows.');
+    }
+    var result = new Matrix(a.rows, b.cols);
+    for (var i = 0; i < result.rows; i++) {
+        for (var j = 0; j < result.cols; j++) {
+            var sum = 0;
+            for (var c = 0; c < a.cols; c++) {
+                sum += a.data[i][c] * b.data[c][j];
+            }
+            result.data[i][j] = sum;
+        }
+    }
+    return result;    
+};
+
+Matrix.transpose = function(a) {
+    var result = new Matrix(a.cols, a.rows);
+    for (var i = 0; i < a.rows; i++) {
+        for (var j = 0; j < a.cols; j++) {
+            result.data[j][i] = a.data[i][j];
+        }
+    }
+    return result;
+};
+
+Matrix.subtract = function(a, b) {
+    var result = new Matrix(a.rows, a.cols);
+    for (var i = 0; i < result.rows; i++) {
+        for (var j = 0; j < result.cols; j++) {
+            result.data[i][j] = a.data[i][j] - b.data[i][j];
+       }
+    }
+    return result;
+};
+
+Matrix.map = function(mat, fn) {
+    var result = new Matrix(mat.rows, mat.cols);
+    for (var i = 0; i < mat.rows; i++) {
+        for (var j = 0; j < mat.cols; j++) {
+            var value = mat.data[i][j];
+            result.data[i][j] = fn(value);
+        }
+    } 
+    return result;
+};
+
+
+
+
+// nueral network
+function sigmoid(n) {return 1/(1+Math.exp(-n));}
+function dsigmoid(y) {return y * (1 - y);}
+function nueralNetWork(numI, numH, numO) {
+    this.weights_ih = new Matrix(numH, numI);
+    this.weights_ho = new Matrix(numO, numH);
+    this.weights_ih.randomize();
+    this.weights_ho.randomize();
+    this.biasH = new Matrix(numH, 1);
+    this.biasO = new Matrix(numO, 1);
+    this.biasH.randomize();
+    this.biasO.randomize();
+    this.learningRate = 0.5;
+    
+    this.feedForward = function(input) {
+        var inputs = Matrix.fromArray(input);
+        var hidden = Matrix.multiply(this.weights_ih, inputs);
+        hidden.add(this.biasH);
+        hidden.map(sigmoid);
+        var output = Matrix.multiply(this.weights_ho, hidden);
+        output.add(this.biasO);
+        output.map(sigmoid);
+        return output.toArray();
+    };
+    
+    this.train = function(input_array, targets_array) {
+        var inputs = Matrix.fromArray(input_array);
+        var hidden = Matrix.multiply(this.weights_ih, inputs);
+        hidden.add(this.biasH);
+        hidden.map(sigmoid);
+        var outputs = Matrix.multiply(this.weights_ho, hidden);
+        outputs.add(this.biasO);
+        outputs.map(sigmoid);
+
+        var targets = Matrix.fromArray(targets_array);
+        var outputErrors = Matrix.subtract(targets, outputs);
+        var gradients = Matrix.map(outputs, dsigmoid);
+        gradients.multiply(outputErrors);
+        gradients.multiply(this.learningRate);
+
+        
+        var hiddenT = Matrix.transpose(hidden);
+        var weights_ho_deltas = Matrix.multiply(gradients, hiddenT);
+        this.weights_ho.add(weights_ho_deltas);
+        this.biasO.add(gradients);
+        
+        var who_t = Matrix.transpose(this.weights_ho);
+        var hiddenErrors = Matrix.multiply(who_t, outputErrors);
+        
+        var hidden_gradients = Matrix.map(hidden, dsigmoid);
+        hidden_gradients.multiply(hiddenErrors);
+        hidden_gradients.multiply(this.learningRate);
+        
+        
+        var inputsT = Matrix.transpose(inputs);
+        var weights_ih_deltas = Matrix.multiply(hidden_gradients, inputsT);
+        
+        this.weights_ih.add(weights_ih_deltas);
+        this.biasH.add(hidden_gradients);
+        
+        
+    };
+    
+    this.setLearningRate = function(l) {this.learningRate = l;};
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // SPY FUNCTION
 
 
